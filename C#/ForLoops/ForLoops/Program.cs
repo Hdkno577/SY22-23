3f0214a42774f3e42332af1e6561951f17f26764
Henry K <hdkno577@learner.dvusd.org>
2022-10-19 08:41:13 -0700

    more bunch of stuff

--- C#/ForLoops/ForLoops/Program.cs
+++ C#/ForLoops/ForLoops/Program.cs
@@ -24,11 +24,67 @@ namespace ForLoops
                 for (int j = 0; j < 1; j++)
                 {
                     Write("*");
-                }
-                for (int j = 0; j < 1; j++)
-                {
+                    WriteLine();
                     Write("**");
+                    WriteLine();
+                    Write("***");
+                    WriteLine();
+                    Write("****");
+                    WriteLine();
+                    Write("*****");
                 }
+                WriteLine();
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Write("    *");
+                WriteLine();
+                Write("   **");
+                WriteLine();
+                Write("  ***");
+                WriteLine();
+                Write(" ****");
+                WriteLine();
+                Write("*****");
+                WriteLine();
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Write("    *    ");
+                WriteLine();
+                Write("   ***   ");
+                WriteLine();
+                Write("  *****  ");
+                WriteLine();
+                Write(" ******* ");
+                WriteLine();
+                Write("*********");
+                WriteLine();
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Write("    1    ");
+                WriteLine();
+                Write("   222   ");
+                WriteLine();
+                Write("  33333  ");
+                WriteLine();
+                Write(" 4444444 ");
+                WriteLine();
+                Write("555555555");
+                WriteLine();
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Write("    1    ");
+                WriteLine();
+                Write("   212   ");
+                WriteLine();
+                Write("  32123  ");
+                WriteLine();
+                Write(" 4321234 ");
+                WriteLine();
+                Write("543212345");
                 WriteLine();
             }
             ReadKey();
